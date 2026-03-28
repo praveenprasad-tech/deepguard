d4878abfe0981130cde4ccdddeff635b8cc38678
Praveen Prasad <your-github-email@gmail.com>
2026-03-28 12:35:14 +0530

    image display added

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import cv2
 
-# image load test
 img = cv2.imread("test.jpg")
 
 if img is None:
     print("Image not found ❌")
 else:
-    print("Image loaded successfully ✅")
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
